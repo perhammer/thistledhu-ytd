d87f3c9c8107ff3b7f3c888495510f76e119b3d7
perhammer <github@perhammer.com>
2018-12-24 07:38:08 +0000

    Refactored the configuration values

--- ytd.sh
+++ ytd.sh
@@ -4,6 +4,10 @@
 # brew install imagemagick
 # brew install inetutils
 
+NETRC_FILE=".netrc_thistledhu"
+FRAME_DELAY_TICKS=40 # there are 100 ticks/second
+ANIMATION_LOOP_COUNT=0 # 0 = loop forever
+
 function ftpSteps() {
 	cat << STOP
 bin
@@ -12,15 +16,15 @@ STOP
 }
 
 function upload() {
-	ftpSteps $* | gftp -i --netrc=.netrc_thistledhu thistledhu.net
+	ftpSteps $* | gftp -i --netrc=$NETRC_FILE thistledhu.net
 }
 
 function grabAndAppend() {
 	local outfile="camera$1-`date +%Y-%m-%d-%H:%M:%S`.jpg"
 	local infiles="camera$1-`date +%Y`-*.jpg"
 	local renderfile="camera$1-`date +%Y`-ytd.gif"
-	curl -s -S --netrc-file .netrc_thistledhu -o $outfile http://camera$1/image.jpg
-	convert -delay 40 -loop 0 $infiles $renderfile
+	curl -s -S --netrc-file $NETRC_FILE -o $outfile http://camera$1/image.jpg
+	convert -delay $FRAME_DELAY_TICKS -loop $ANIMATION_LOOP_COUNT $infiles $renderfile
 	ln -sf $renderfile camera$1-ytd.gif 
 }
 
